1d469118e4bf712f5957a8a24032ef79d3b17206
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-10-16 18:46:25 -0400

    s24-379 Category Service

--- src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Create by dmitri on 2022-10-11.
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
 }
---
+++ src/test/java/guru/springfamework/services/CategoryServiceTest.java
@@ -0,0 +1,65 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class CategoryServiceTest {
+
+    public static final Long ID = 2L;
+    public static final String NAME = "Jimmy";
+    CategoryService categoryService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+    }
+
+    @Test
+    void getAllCategories() {
+        //given
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        //when
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+
+        //then
+        assertEquals(3, categoryDTOS.size());
+    }
+
+    @Test
+    void getCategoryByName() {
+        //given
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+        //when
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+        //then
+        assertEquals(ID, categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
