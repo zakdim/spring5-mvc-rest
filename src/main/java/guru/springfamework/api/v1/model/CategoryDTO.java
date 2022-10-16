e1540a27ef4b4c5af7a503d7884f0e3c963a4250
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-10-16 17:27:19 -0400

    s24-377 Using MapStruct

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,11 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Data;
+
 /**
  * Create by dmitri on 2022-10-11.
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
