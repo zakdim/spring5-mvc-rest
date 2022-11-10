3c11531736b4fcb06abc4ea124d388eb98f48e6d
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-11-09 21:59:04 -0500

    s24-388 Delete Customer

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -93,4 +93,9 @@ public class CustomerServiceImpl implements CustomerService {
 
         }).orElseThrow(RuntimeException::new); //TODO implement better exception handling;
     }
+
+    @Override
+    public void deleteCustomerById(Long id) {
+        customerRepository.deleteById(id);
+    }
 }
--- src/test/java/guru/springfamework/services/CustomerServiceImplTest.java
+++ src/test/java/guru/springfamework/services/CustomerServiceImplTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class CustomerServiceImplTest {
 
@@ -115,5 +115,15 @@ class CustomerServiceImplTest {
         //then
         assertEquals(customerDTO.getFirstName(), savedDto.getFirstName());
         assertEquals("/api/v1/customers/1", savedDto.getCustomerUrl());
+    }
+
+
+    @Test
+    void deleteCustomerById() throws Exception {
+        Long id = 1L;
+
+        customerRepository.deleteById(id);
+
+        verify(customerRepository, times(1)).deleteById(anyLong());
     }
 }
