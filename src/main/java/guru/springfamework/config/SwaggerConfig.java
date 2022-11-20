560d746ea7fa08af075b565638bdb56b2bbe1fbd
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-11-20 13:48:04 -0500

    s25-402 Assignment Review

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package guru.springfamework.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
